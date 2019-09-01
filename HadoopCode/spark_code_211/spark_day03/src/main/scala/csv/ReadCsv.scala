45b29729c2ec8258accb3ac54efac44677d3d275
sakura <872860382@qq.com>
2019-09-01 08:31:13 +0800

    9.01

--- HadoopCode/spark_code_211/spark_day03/src/main/scala/csv/ReadCsv.scala
+++ HadoopCode/spark_code_211/spark_day03/src/main/scala/csv/ReadCsv.scala
@@ -1,6 +1,6 @@
 package csv
 
-import org.apache.spark.sql.{DataFrame, Dataset,SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SaveMode, SparkSession}
 import org.apache.spark.sql.types._
 
 object ReadCsv {
@@ -22,15 +22,7 @@ object ReadCsv {
     }
     import spark.implicits._   //spark是一个对象  =====val spark: SparkSession
 
-    //无head的csv
 
-    val dataFrame = spark.read
-      .option("sep", "\\t")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .csv("datas/ml-100k/u.dat")
-    dataFrame.printSchema()
-    dataFrame.show(10)
 
     val schema = StructType(
       Array(
@@ -41,16 +33,51 @@ object ReadCsv {
       )
     )
 
+
+    //无head的csv  当读取CSV格式数据时，如果文件首行不是列名称，自定Schema信息
+
     val mlsDF: DataFrame = spark.read
       .schema(schema)
       .option("sep", "\\t")
       .csv("datas/ml-100k/u.data")
     mlsDF.printSchema()
     mlsDF.show(10)
+println("===============================================================")
+
+    //第一行是head的CSV的文件，，，，简单年多了…………………………………………………………………………………………………………………………
+    val dataFrame = spark.read
+      .option("sep", "\\t")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("datas/ml-100k/u.dat")
+    dataFrame.printSchema()
+    dataFrame.show(10)
 
 
 
+    println("==============================================================")
+    mlsDF
+      // 降低分区数为1，此时保存JSON文件就是一个文件
+      .coalesce(1)
+      .write
+      .mode(SaveMode.Overwrite) // 设置保存模式
+      .json("datas/ml-json/")
 
+
+    spark.read    //链式编程————————————————————————————————————————————————————保存到文件
+      .option("sep", "\\t")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("datas/ml-100k/u.dat")
+      .coalesce(1)
+      .write
+      .mode(SaveMode.Overwrite) // 设置保存模式
+      .json("datas/ml-json/")
+
+
+
+    //关闭应用
+    spark.stop()
   }
 
 }
